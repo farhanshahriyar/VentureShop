02de0493110e6acde1a07f48cbd85a0e60acf195
farhanshahriyar <farhan.abir9999@gmail.com>
2022-10-19 20:01:38 +0600

    fixed bugs

--- backend/data/user.js
+++ backend/data/user.js
@@ -1,21 +1,22 @@
-import bcrypt from "bcryptjs/dist/bcrypt"
-const users =[
-    {
-        name: 'Admin User',
-        email: 'admin@example.com',
-        password: bcrypt.hashSync('123456', 10),
-        isAdmin: true
-    },
-    {
-        name: 'Demo',
-        email: 'demo@example.com',
-        password: bcrypt.hashSync('123456', 10),
-    },
-    {
-        name: 'test',
-        email: 'test@example.com',
-        password: bcrypt.hashSync('123456', 10),
-    },
+import bcrypt from 'bcryptjs'
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'john@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
+  {
+    name: 'Jane Doe',
+    email: 'jane@example.com',
+    password: bcrypt.hashSync('123456', 10),
+  },
 ]
 
 export default users
--- backend/seeder.js
+++ backend/seeder.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import users from './data/user'
@@ -8,50 +8,99 @@ import Product from './models/productModel'
 import Order from './models/oderModel'
 import connectDB from './config/db'
 
+// dotenv.config()
+
+// connectDB()
+
+// const importData = async () => {
+//     try{
+//         await Order.deleteMany()
+//         await Product.deleteMany()
+//         await User.deleteMany()
+//         // await User.insertMany(users)
+
+//         const createdUsers = await User.insertMany(users)
+//         const adminUser= createdUsers[0]._id
+//         const sampleProducts = products.map(product => {
+//             return {...product, user: adminUser}
+//         })
+
+//         await Product.insertMany(sampleProducts)
+
+//         console.log('Data Imported!'.green.inverse)
+//         process.exit()
+//     }catch(error){
+//         console.error(`${error}`.red.inverse)
+//         process.exit(1)
+//     }
+// }
+
+// const destroyData = async () => {
+//     try{
+//         await Order.deleteMany()
+//         await Product.deleteMany()
+//         await User.deleteMany()
+//         // await User.insertMany(users)
+
+//         console.log('Data Destroyed!'.red.inverse)
+//         process.exit()
+//     }catch(error){
+//         console.error(`${error}`.red.inverse)
+//         process.exit(1)
+//     }
+// }
+
+// if (process.argv[2] === '-d') {
+//     destroyData()
+// }else {
+//     importData()
+// }
+
+
 dotenv.config()
 
 connectDB()
 
 const importData = async () => {
-    try{
-        await Order.deleteMany()
-        await Product.deleteMany()
-        await User.deleteMany()
-        // await User.insertMany(users)
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
 
-        const createdUsers = await User.insertMany(users)
-        const adminUser= createdUsers[0]._id
-        const sampleProducts = products.map(product => {
-            return {...product, user: adminUser}
-        })
+    const createdUsers = await User.insertMany(users)
 
-        await Product.insertMany(sampleProducts)
+    const adminUser = createdUsers[0]._id
 
-        console.log('Data Imported!'.green.inverse)
-        process.exit()
-    }catch(error){
-        console.error(`${error}`.red.inverse)
-        process.exit(1)
-    }
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser }
+    })
+
+    await Product.insertMany(sampleProducts)
+
+    console.log('Data Imported!'.green.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
 }
 
 const destroyData = async () => {
-    try{
-        await Order.deleteMany()
-        await Product.deleteMany()
-        await User.deleteMany()
-        // await User.insertMany(users)
+  try {
+    await Order.deleteMany()
+    await Product.deleteMany()
+    await User.deleteMany()
 
-        console.log('Data Destroyed!'.red.inverse)
-        process.exit()
-    }catch(error){
-        console.error(`${error}`.red.inverse)
-        process.exit(1)
-    }
+    console.log('Data Destroyed!'.red.inverse)
+    process.exit()
+  } catch (error) {
+    console.error(`${error}`.red.inverse)
+    process.exit(1)
+  }
 }
 
 if (process.argv[2] === '-d') {
-    destroyData()
-}else {
-    importData()
+  destroyData()
+} else {
+  importData()
 }
